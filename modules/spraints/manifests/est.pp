30b760a5774071d2adadaa51a9ff952e9d453c4f
Matt Burke <spraints@gmail.com>
2014-11-13 14:55:52 -0500

    sigh

--- modules/spraints/manifests/est.pp
+++ modules/spraints/manifests/est.pp
@@ -1,6 +1,5 @@
 class spraints::est {
   file { "/etc/localtime":
     ensure => "/usr/share/zoneinfo/America/Indiana/Indianapolis",
-    require => File["/usr/share/zoneinfo/America/Indiana/Indianapolis"],
   }
 }
