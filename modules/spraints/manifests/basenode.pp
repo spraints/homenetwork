4ace906522f06293fd53272527eb9bb897163c89
Matt Burke <spraints@gmail.com>
2014-11-16 20:59:32 -0500

    Include some fake hostnames.

--- modules/spraints/manifests/basenode.pp
+++ modules/spraints/manifests/basenode.pp
@@ -1,4 +1,10 @@
 class spraints::basenode {
   include spraints::est
   include spraints::services::ntp
+
+  file { "/etc/hosts":
+    ensure => present,
+    mode => 644,
+    source => "puppet:///modules/spraints/etc/hosts",
+  }
 }
