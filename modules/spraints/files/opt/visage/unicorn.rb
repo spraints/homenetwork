25361a1abe0787ae6d19d7215bd1fd298f6c86c7
Matt Burke <spraints@gmail.com>
2014-11-16 21:05:47 -0500

    meh. it'll never need to restart, anyway.

--- modules/spraints/files/opt/visage/unicorn.rb
+++ modules/spraints/files/opt/visage/unicorn.rb
@@ -1,16 +1,5 @@
 preload_app true
 worker_processes 4
-pid "/var/run/visage-app-unicorn.pid"
-
-before_fork do |server, worker|
-  old_pid = "/var/run/visage-app-unicorn.pid.oldbin"
-  if File.exists?(old_pid) && old_pid != server.pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-end
 
 after_fork do |server, worker|
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
