75192375d94f284040a43b99f29011d58938a566
Matt Burke <spraints@gmail.com>
2014-11-24 12:11:50 -0500

    More notes.

--- modules/spraints/manifests/role/weater_station.pp
+++ modules/spraints/manifests/role/weater_station.pp
@@ -5,4 +5,17 @@ class spraints::role::weather_station {
 
   # todo - fowsr + collectd
   # todo - fowsr + wunderground (KINPICKA2)
+
+  # ruby controller.rb
+  # - listen on a socket
+  # - run fowsr
+  # - foward data to the socket
+  #
+  # ruby collectd-fowsr.rb
+  # - open the socket
+  # - write the required data
+  #
+  # ruby collectd-wunderground.sh
+  # - open the socket
+  # - every Ns report the last seen values to wunderground.
 }
