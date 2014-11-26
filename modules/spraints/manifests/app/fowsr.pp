a9d106fc399da00f44bef128691539b80c401bf7
Matt Burke <spraints@gmail.com>
2014-11-26 05:55:32 -0500

    avoid a tight read loop.

--- modules/spraints/manifests/app/fowsr.pp
+++ modules/spraints/manifests/app/fowsr.pp
@@ -4,7 +4,7 @@ class spraints::app::fowsr {
     provider => git,
     user => "root",
     source => "https://github.com/spraints/fowsr",
-    revision => "9bafa4d8d50b1288d8c0bdd2217100f77655c429",
+    revision => "baa403e2d5d9ef9df81860ae17b22882e2294606",
   }
 
   package { "libusb-dev":
