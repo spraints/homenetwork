62fd0a9e78a6b517afe03da52c79a8c65ed700c1
Matt Burke <spraints@gmail.com>
2015-01-01 10:44:04 -0500

    Start adding crashplan.

---
+++ modules/spraints/manifests/role/crashplan.pp
@@ -0,0 +1,6 @@
+class spraints::role::crashplan {
+  # Download http://download1.us.code42.com/installs/linux/install/CrashPlan/CrashPlan_3.6.4_Linux.tgz
+  # Install it.
+  # Configure it.
+  # Mount a disk?
+}
