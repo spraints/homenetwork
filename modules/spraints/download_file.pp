32fd3b1257ffbd27a20f9cde210ff3ea13cb338d
Matt Burke <spraints@gmail.com>
2015-01-02 08:06:04 -0500

    Download crashplan.

---
+++ modules/spraints/download_file.pp
@@ -0,0 +1,8 @@
+define download_file($url = "", $cwd = "", $require = "") {
+  exec { "download ${name}":
+    command => "curl -L -o \"${name}.tmp\" \"${url}\" && mv \"${name}.tmp\" \"${name}\"",
+    cwd => $cwd,
+    require => $require,
+    creates => "${cwd}/${name}",
+  }
+}
--- modules/spraints/manifests/role/crashplan.pp
+++ modules/spraints/manifests/role/crashplan.pp
@@ -1,4 +1,9 @@
 class spraints::role::crashplan {
+  download_file {
+    "CrashPlan_3.6.4_Linux.tgz":
+      url => "http://download1.us.code42.com/installs/linux/install/CrashPlan/CrashPlan_3.6.4_Linux.tgz",
+      cwd => "/var/local/homenetwork-puppet/downloads",
+  }
   # Download http://download1.us.code42.com/installs/linux/install/CrashPlan/CrashPlan_3.6.4_Linux.tgz
   # Install it.
   # Configure it.
