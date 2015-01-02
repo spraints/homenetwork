68a328a9d0cde5dc57dd2020cdb7b7936ec479ab
Matt Burke <spraints@gmail.com>
2015-01-02 08:19:34 -0500

    fully-qualified paths.

--- modules/spraints/manifests/util/download_file.pp
+++ modules/spraints/manifests/util/download_file.pp
@@ -1,6 +1,6 @@
 define spraints::util::download_file($url = "", $cwd = "") {
   exec { $name:
-    command => "curl -L -o \"${name}.tmp\" \"${url}\" && mv \"${name}.tmp\" \"${name}\"",
+    command => "/usr/bin/curl -L -o \"${name}.tmp\" \"${url}\" && /bin/mv \"${name}.tmp\" \"${name}\"",
     cwd => $cwd,
     creates => "${cwd}/${name}",
   }
