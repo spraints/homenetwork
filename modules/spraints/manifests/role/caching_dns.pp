5ab08938709d8ab71fb17ba25ddb6853bb617744
Matt Burke <spraints@gmail.com>
2015-08-19 08:07:44 -0400

    Don't need to specify the upstream

--- modules/spraints/manifests/role/caching_dns.pp
+++ modules/spraints/manifests/role/caching_dns.pp
@@ -1,7 +1,5 @@
 # Loosely based on https://www.digitalocean.com/community/tutorials/how-to-configure-bind-as-a-caching-or-forwarding-dns-server-on-ubuntu-14-04
-class spraints::role::caching_dns(
-  $upstream = "8.8.8.8",
-) {
+class spraints::role::caching_dns {
 
   package { ["bind9", "bind9utils", "bind9-doc"]:
     ensure => installed
