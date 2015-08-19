cdace46b2576e5c896b40d269188b6d66197b1c3
Matt Burke <spraints@gmail.com>
2015-08-19 08:13:53 -0400

    Add a note about flushing the cache

--- modules/spraints/manifests/role/caching_dns.pp
+++ modules/spraints/manifests/role/caching_dns.pp
@@ -1,6 +1,10 @@
 # Loosely based on https://www.digitalocean.com/community/tutorials/how-to-configure-bind-as-a-caching-or-forwarding-dns-server-on-ubuntu-14-04
 class spraints::role::caching_dns {
 
+  # Info on cache flushing is here:
+  # https://kb.isc.org/article/AA-01002/0/How-do-I-flush-or-delete-incorrect-records-from-my-recursive-server-cache.html
+  # tldr - use `rndc`
+
   package { ["bind9", "bind9utils", "bind9-doc"]:
     ensure => installed
   }
