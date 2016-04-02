4a86190f9394fdb9ec5e3260767c95c0b907ca43
Matt Burke <spraints@gmail.com>
2016-04-02 12:14:35 -0400

    Collect att info on router

--- manifests/zig-or-att.pp
+++ manifests/zig-or-att.pp
@@ -3,4 +3,7 @@ node /^zig-or-att/ {
 
   include spraints::role::router
   include spraints::role::sprouter_config
+
+  # Probably temporary, until another mini is available
+  include spraints::role::att_wireless
 }
--- modules/spraints/manifests/role/att_wireless.pp
+++ modules/spraints/manifests/role/att_wireless.pp
@@ -2,14 +2,14 @@ class spraints::role::att_wireless(
   $router_ip = "192.168.0.1",
   $login = "attadmin",
 ) {
-  include spraints::services::collectd
+  #include spraints::services::collectd
 
   file { "/etc/collectd/collectd.conf.d/att_wireless.conf":
     ensure => present,
     mode   => 644,
     owner  => "root",
     source => "puppet:///modules/spraints/etc/collectd/collectd.conf.d/att_wireless.conf",
-    notify => Service["collectd"],
+    #notify => Service["collectd"],
   }
 
   file { "/opt/collectd/att_wireless":
@@ -21,7 +21,7 @@ class spraints::role::att_wireless(
       File["/opt/collectd"],
       Package["jq"],
     ],
-    notify => Service["collectd"],
+    #notify => Service["collectd"],
   }
 
   file { "/opt/collectd":
@@ -36,14 +36,14 @@ class spraints::role::att_wireless(
 
   #####
 
-  include spraints::services::visage
+  #include spraints::services::visage
 
-  file { "/var/local/visage/profiles.yaml.d/att_wireless.yaml":
-    ensure => present,
-    owner => "visage",
-    group => "visage",
-    mode => 644,
-    content => template("spraints/var/local/visage/profiles.yaml.d/att_wireless.yaml.erb"),
-    notify => Exec["build visage profile"],
-  }
+  #file { "/var/local/visage/profiles.yaml.d/att_wireless.yaml":
+  #  ensure => present,
+  #  owner => "visage",
+  #  group => "visage",
+  #  mode => 644,
+  #  content => template("spraints/var/local/visage/profiles.yaml.d/att_wireless.yaml.erb"),
+  #  notify => Exec["build visage profile"],
+  #}
 }
