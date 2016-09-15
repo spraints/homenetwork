c5b023b045a2b4c43dcaaad5d7e0bcf1aaf610be
Matt Burke <spraints@gmail.com>
2016-09-15 09:56:12 -0400

    rm att device metrics

--- manifests/minibuntu.pp
+++ manifests/minibuntu.pp
@@ -3,7 +3,8 @@ node /^minibuntu/ {
 
   include spraints::role::network_monitor
   include spraints::role::weather_station
-  include spraints::role::att_wireless
   include spraints::role::caching_dns
   include spraints::role::monitor_zig_or_att
+
+  include spraints::role::att_wireless_tombstone
 }
